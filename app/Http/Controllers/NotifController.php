656dfb78e25c430b72324065dd92dc3033eeee8f
VinaAlvionita1 <vinaalvionita9917@gmail.com>
2022-01-18 19:15:54 +0700

    18 Januari 2022

--- app/Http/Controllers/NotifController.php
+++ app/Http/Controllers/NotifController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,17 +23,93 @@ class NotifController extends Controller
 
     public function index()
     {
-        $hasil = [];
+        $output = [];
         $proyek = DB::select('select * from proyek');
         if(!empty($proyek)){
             foreach ($proyek as $a) {
                 $data = [
                     'nama_proyekk' => $a->nama_proyek,
-                    'tgl_selesai_proyek' => $a->tgl_selesai_proyek
+                    'tgl_selesai_proyek' => $a->tgl_selesai_proyek,
+                    'notification' => []
                 ];
-                $hasil[] = $data;
+                $tgl_selesai = date('Y-m-d', strtotime($a->tgl_selesai_proyek));
+                $now = date('Y-m-d', strtotime("now"));
+                $selisih = date('Y-m-d', strtotime('-7 days', strtotime($tgl_selesai)));
+                $h1 = date('Y-m-d', strtotime('-1 days', strtotime($tgl_selesai)));
+                // $diff = $now - $selisih;
+                // $perbedaan = floor($diff / (60 * 60 * 24)) . ' Hari';
+                $dStart = new DateTime($now);
+                $dEnd = new DateTime($selisih);
+                $dDiff = $dStart->diff($dEnd);
+                $x = $dDiff->d;
+                if (strtotime($now) <= strtotime($tgl_selesai)){
+                    $s = $now . " lebih kecil dari". $tgl_selesai ;
+                    $hasil = [
+                        'tgl_selesai' => $tgl_selesai,
+                        'tgl_sekarang' => $now,
+                        'h-7' => $selisih,
+                        'h-1' => $h1,
+                        'perbedaan' => $x,
+                        'pesan' => $a->nama_proyek . " Kurang " . $x . "Deadline Proyek!",
+                    ];
+                    $output[] = $hasil;
+                }
+                // $task = DB::select('select * from notification where id_proyek = ?', [$a->id_proyek]);
+                // if( strtotime($now) <= strtotime($tgl_selesai) ){
+                //     if( strtotime($tgl_selesai) == strtotime($now)){
+                //         // $add = Notification::create([
+                //         //     'id_proyek' => $a->id_proyek,
+                //         //     'tgl_notif' => $now,
+                //         //     'pesan' => $a->nama_proyek . " Hari Ini Deadline Proyek!",
+                //         // ]);
+                //         $hasil['notification'][] = [
+                //             // 'pesan' => $add->pesan
+                //             'tgl_selesai' => $tgl_selesai,
+                //             'tgl_sekarang' => $now,
+                //             'h-7' => $selisih,
+                //             'h-1' => $h1,
+                //             'perbedaan' => $x,
+                //             'pesan' => $a->nama_proyek . " Hari Ini Deadline Proyek!",
+                //         ];
+                //         $output[] = $hasil;
+                //     }
+                //     if( strtotime($h1) == strtotime($tgl_selesai)){
+                //         // $add = Notification::create([
+                //         //     'id_proyek' => $a->id_proyek,
+                //         //     'tgl_notif' => $now,
+                //         //     'pesan' => $a->nama_proyek . " Besok Deadline Proyek!",
+                //         // ]);
+                //         $hasil['notification'][] = [
+                //             // 'pesan' => $add->pesan
+                //             'tgl_selesai' => $tgl_selesai,
+                //             'tgl_sekarang' => $now,
+                //             'h-7' => $selisih,
+                //             'h-1' => $h1,
+                //             'perbedaan' => $x,
+                //             'pesan' => $a->nama_proyek . " Besok Deadline Proyek!",
+                //         ];
+                //         $output[] = $hasil;
+                //     }
+                //     if( strtotime($now) <= strtotime($selisih)){
+                //         // $add = Notification::create([
+                //         //     'id_proyek' => $a->id_proyek,
+                //         //     'tgl_notif' => $now,
+                //         //     'pesan' => $a->nama_proyek . " Kurang " . $perbedaan . "Deadline Proyek!",
+                //         // ]);
+                //         $hasil['notification'][] = [
+                //             // 'pesan' => $add->pesan
+                //             'tgl_selesai' => $tgl_selesai,
+                //             'tgl_sekarang' => $now,
+                //             'h-7' => $selisih,
+                //             'h-1' => $h1,
+                //             'perbedaan' => $x,
+                //             'pesan' => $a->nama_proyek . " Kurang " . $x . "Deadline Proyek!",
+                //         ];
+                //         $output[] = $hasil;
+                //     }
+                // }
             }
         }
-        return response()->json($hasil);
+        return response()->json($output);
     }
 }
---
+++ database/migrations/2022_01_14_114307_create_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id_notification');
+            $table->unsignedInteger('id_proyek');
+            $table->foreign('id_proyek')->references('id_proyek')->on('proyek');
+            $table->string('tgl_notif');
+            $table->string('pesan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
